9722ba6d851cb06bbe65bd4b090a224258ba7106
dislersd <dislersd@gmail.com>
2020-08-08 14:48:04 -0700

    conditional checks for ui state

--- src/App.tsx
+++ src/App.tsx
@@ -66,9 +66,11 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
